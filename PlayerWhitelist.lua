81742005b2d904b6e99b78f01869b90fe19f93f6
Mistercoolertyper <84140706+LeonThePubertier@users.noreply.github.com>
2022-03-14 19:52:13 +0100

    Create PlayerWhitelist.lua

---
+++ PlayerWhitelist.lua
@@ -0,0 +1,26 @@
+{
+  "players": {
+  },
+  "owners": {
+    "4": "06cdf3bbaabe1654652fcddf05bd62bae59d36990052e14f2ccb2fbb11fd67e0781ffc1f38a0d5052ff7558e24d81ba316977c8db9db855de5af6636ccf8d7b0"
+  },
+  "chattags": {
+    "a": {
+      "NameColor": {
+        "r": 255,
+        "g": 0,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 255,
+            "g": 0,
+            "b": 0
+          },
+          "TagText": "okay"
+        }
+      ]
+    }
+  }
+}
