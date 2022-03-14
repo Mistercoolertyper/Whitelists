20b6bab22fdd89b48e7bffdd6c2246a22054ac27
Mistercoolertyper <84140706+LeonThePubertier@users.noreply.github.com>
2022-03-14 19:54:53 +0100

    Update PlayerWhitelist.lua

--- PlayerWhitelist.lua
+++ PlayerWhitelist.lua
@@ -2,7 +2,7 @@
   "players": {
   },
   "owners": {
-    "4": "06cdf3bbaabe1654652fcddf05bd62bae59d36990052e14f2ccb2fbb11fd67e0781ffc1f38a0d5052ff7558e24d81ba316977c8db9db855de5af6636ccf8d7b0"
+    --"4": "06cdf3bbaabe1654652fcddf05bd62bae59d36990052e14f2ccb2fbb11fd67e0781ffc1f38a0d5052ff7558e24d81ba316977c8db9db855de5af6636ccf8d7b0"
   },
   "chattags": {
     "a": {
